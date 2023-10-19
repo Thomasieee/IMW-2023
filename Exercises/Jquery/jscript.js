449817923233a06e0fc4ce754adda72eba53873a
Thomasieee <nikhilthomas.id@gmail.com>
2023-10-19 17:42:52 -0400

    chnages

--- Exercises/Jquery/jscript.js
+++ Exercises/Jquery/jscript.js
@@ -1,1 +1,8 @@
 console.log("hello");
+
+for (let step = 0; step < 1000; step++){
+    let blocks = '<div class="block" styles="width:100vw; height: 10px; border: 2px solid black; ">';
+    $
+
+   
+}
