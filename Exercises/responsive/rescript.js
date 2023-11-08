b773046c8f4253ef3d3888847d2d170dc03b9d04
Thomasieee <nikhilthomas.id@gmail.com>
2023-11-08 09:09:54 -0500

    ch

---
+++ Exercises/responsive/rescript.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  if (window.innerWidth <= 600) {
+    document.getElementById("#heading").textContent = "noooooo";
+  }
+
+  if (window.innerHeight <= 600) {
+    document.getElementById("#heading").textContent = "seriously?";
+  }
+});
+
+console.log("This was .....");
