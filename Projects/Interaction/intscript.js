20cc09b88905179dab8e5b9d788116d0fa180130
Thomasieee <nikhilthomas.id@gmail.com>
2023-11-19 10:41:25 -0500

    changes

--- Projects/Interaction/intscript.js
+++ Projects/Interaction/intscript.js
@@ -11,76 +11,76 @@ $(document).ready(function () {
     "mango",
     "peach",
   ];
-  let intervalDuration = 5000; // Initial interval duration in milliseconds
-  let rectInterval; // Variable to hold the interval reference
-  let currentRect;
+
+  let intervalDuration = 5000;
+  let rectInterval;
+  let score = 0;
+  let animationDuration = 10000;
 
   function createRectangle() {
     const height = Math.floor(Math.random() * 60) + 20;
     const randomWord = words[Math.floor(Math.random() * words.length)];
-
-    const rect = $('<div class="rect"></div>').css({
-      width: $("#container").width(),
-      height: height + "px",
-      top: "0",
-    });
-
-    // Set the random word as text content of the rectangle
-    rect.text(randomWord);
+    const rect = $('<div class="rect"></div>')
+      .css({
+        width: $("#container").width(),
+        height: height + "px",
+        top: "0",
+      })
+      .text(randomWord);
 
     const $container = $("#container");
     const existingRects = $container.find(".rect");
-    let stackHeight = 0;
-
-    existingRects.each(function () {
-      stackHeight += $(this).outerHeight();
-    });
+    let stackHeight = existingRects
+      .toArray()
+      .reduce((acc, el) => acc + $(el).outerHeight(), 0);
 
     rect.css("bottom", stackHeight + "px");
     $container.append(rect);
 
     const rectTop = $container.height() - stackHeight - rect.outerHeight();
-    rect.animate(
+    const animation = rect.animate(
       { top: rectTop },
       {
-        duration: 10000,
+        duration: animationDuration,
+        easing: "linear",
         step: function () {
           const rectOffset = rect.offset().top;
-          const containerOffset = $container.offset().top * 0.8;
+          const containerOffset = $container.offset().top;
 
           if (rectOffset <= containerOffset) {
-            rect.stop();
+            animation.stop();
             $(".game-over").show();
             clearInterval(rectInterval);
           }
         },
+        complete: function () {
+          $(this).data("isAnimating", false);
+        },
       }
     );
-    rect.data("word", randomWord);
-    currentRect = rect;
+
+    rect.data("word", randomWord).data("isAnimating", true);
+
+    setInterval(() => {
+      animationDuration -= 100;
+      animation.duration = animationDuration;
+    }, 10000);
   }
 
-  function decreaseInterval() {
-    intervalDuration -= 3000; // Decrease interval duration by 1 second
-    clearInterval(rectInterval); // Clear the current interval
-    rectInterval = setInterval(createRectangle, intervalDuration); // Start new interval
-  }
-
-  // Create rectangles at the initial interval duration
   rectInterval = setInterval(createRectangle, intervalDuration);
 
-  // Decrease interval duration every 10 seconds
-  setInterval(decreaseInterval, 50000);
   $("#typedContent").on("input", function () {
     const typedText = $(this).val().trim().toLowerCase();
 
     $(".rect").each(function () {
       const wordInRect = $(this).data("word").toLowerCase();
+      const isAnimating = $(this).data("isAnimating");
 
-      if (typedText === wordInRect) {
+      if (typedText === wordInRect && isAnimating) {
         $(this).remove();
-        $(this).stop();
-        $("#typedContent").val(""); // Clear typed text upon removing the rectangle
+        $("#typedContent").val("");
+        score++;
+        $("#score").text(`Score: ${score}`);
       }
     });
   });
@@ -88,7 +88,7 @@ $(document).ready(function () {
   $("#typedContent").keydown(function (e) {
     if (e.key === "Enter") {
       e.preventDefault();
-      $(this).val(""); // Clear typed text upon pressing "Enter"
+      $(this).val("");
     }
   });
 });
